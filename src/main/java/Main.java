557c4d16c2468c7ced87fa42a942c37b2edbdeb8
ZeroM2 <kemy21@naver.com>
2022-07-12 02:18:59 +0900

    usr/article/detail 명령으로 최근 게시물 보기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,25 @@ public class Main {
 
         System.out.println("== 프로그램 시작 ==");
         int articlesLastId = 0;
+        Article lastArticle =null;
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
             if (cmd.equals("exit")) {
                 break;
+            } else if (cmd.equals("/usr/article/detail")) {
+
+                if ( lastArticle == null) {
+                    System.out.println("게시물이 존재하지 않습니다..");
+                    continue;
+                }
+                Article article = lastArticle;
+                System.out.println("=게시물 상세내용=");
+                System.out.printf("번호 : %d\n", article.id);
+                System.out.printf("제목 : %s\n ", article.title);
+                System.out.printf("내용 : %s\n", article.body);
+
+
             } else if (cmd.equals("/usr/article/write")) {
                 System.out.println("=게시물 등록=");
                 System.out.println("제목 : ");
@@ -22,6 +36,8 @@ public class Main {
                 articlesLastId = id;
 
                 Article article = new Article(id, title, body);
+                lastArticle =article;
+
 
                 System.out.println("생성된 게시물 객체 : " + article);
 
@@ -46,7 +62,8 @@ class Article {
     String title;
     String body;
 
-    Article(int id, String title, String body){
+    Article(int id, String title, String body) {
+
         this.id = id;
         this.title = title;
         this.body = body;
